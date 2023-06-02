b200615e0e2648581f7a3f3008abae3250e5e3db
kartik298 <kartikagarwal698@gmail.com>
2023-06-02 23:42:11 +0530

    initial commit

---
+++ src/Component/Score.js
@@ -0,0 +1,20 @@
+
+import { motion } from 'framer-motion';
+const Score = ({ score,chance }) => {
+
+
+  if (chance<=0) {return <>
+  <motion.h1 animate={{ rotateZ:360,}}>Final score:{score} 😁😁</motion.h1></>;  }
+
+    return (
+      <div className="score-board" style ={{backgroundColor:'invisible'} }>
+        <div style={{ fontWeight: 'bold' }}>Game score:</div>
+        <h1>{score}</h1>
+        <div style={{ fontWeight: 'bold' }}>Total chance:</div>
+        <h1>{chance}</h1>
+      </div>git
+
+    )
+    };
+
+  export default Score;
